ccde4aec12d365f1e78944953013860694eef106
Adam Rahman <adamrahmandata@users.noreply.github.com>
2025-11-10 22:28:22 -0500

    trying again

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,25 +30,15 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-pwd
-ls
-cd data
-mkdir raw
-cd raw
-pwd
-mv C:/Users/AdamRahman/dsi3/shell/02_activities/assignments/newproject/rawdata C:/Users/AdamRahman/dsi3/shell/02_activities/assignments/newproject/data/raw
+mv rawdata data/raw
 # 3. List the contents of the ./data/raw directory
-pwd
-ls
+ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-cd ../
-pwd
-ls
-mkdir processed
-cd processed
-mkdir server_logs user_logs event_logs
-pwd
-ls
+
+mkdir -p data/processed/server_logs 
+mkdir -p data/processed/user_logs 
+mkdir -p data/processed/event_logs
+
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ../raw/rawdata
